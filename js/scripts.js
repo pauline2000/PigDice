69bf3b793d2209948b2378b59d64cb128a52d1ae
Sherry <ndungupauline0@gmail.comgit config --global user .name 2000sherrygit config --user .email ndungupauline0@gmail.com>
2018-04-13 00:07:25 +0300

    player constructor

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+// business logic
+// creating a player constructor
+function playerName(turnScore,totalScore,overallScore,playerName){
+    this.turnScore=0;
+    this.totalScore=0;
+    this.overallScore=0; 
+    this.playerName=playerName; 
+}
+
+// user interface
+$(document).ready(function(){
+// jquery methods coming here
+
+}
